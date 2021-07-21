78fdb5ea2362773170b410f1f2249f72d16a2aef
Vadim Guryanov <vadim.guriyanov@flatstack.com>
2021-07-21 18:18:34 +0300

    1.0.0-alpha11

--- demo/src/main/kotlin/dev/hotwire/turbo/demo/features/web/WebBottomSheetFragment.kt
+++ demo/src/main/kotlin/dev/hotwire/turbo/demo/features/web/WebBottomSheetFragment.kt
@@ -1,8 +1,13 @@
 package dev.hotwire.turbo.demo.features.web
 
+import android.webkit.WebResourceRequest
 import dev.hotwire.turbo.demo.base.NavDestination
 import dev.hotwire.turbo.fragments.TurboWebBottomSheetDialogFragment
 import dev.hotwire.turbo.nav.TurboNavGraphDestination
 
 @TurboNavGraphDestination(uri = "turbo://fragment/web/modal/sheet")
-class WebBottomSheetFragment : TurboWebBottomSheetDialogFragment(), NavDestination
+class WebBottomSheetFragment : TurboWebBottomSheetDialogFragment(), NavDestination {
+    override fun createWebResourceRequest(): WebResourceRequest? {
+        return null
+    }
+}
